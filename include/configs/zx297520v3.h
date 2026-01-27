54ac5fb9eace310c669f7491e42b59a18106a36a
rva3 <rva333@protonmail.com>
2026-01-27 02:11:58 +0200

    configs: add zx297520v3 config

---
+++ include/configs/zx297520v3.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * (C) Copyright 2026 Me, myself, and I
+ *
+ */
+
+#ifndef __CONFIG_ZX297520V3_H
+#define __CONFIG_ZX297520V3_H
+
+#define CFG_SYS_SDRAM_BASE 0x20000000
+#define CFG_SYS_INIT_RAM_ADDR	0x20000000
+#define CFG_SYS_INIT_RAM_SIZE	0x8000000
+#define CFG_SYS_HZ_CLOCK 26000000
+
+#endif /* __CONFIG_ZX297520V3_H */
+
