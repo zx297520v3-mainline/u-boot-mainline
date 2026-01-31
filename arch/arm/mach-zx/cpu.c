7c47bd6913a6c9c07468266cc342c7bda37ca112
rva3 <rva333@protonmail.com>
2026-01-31 20:24:29 +0200

    zx: program PLL

--- arch/arm/mach-zx/cpu.c
+++ arch/arm/mach-zx/cpu.c
@@ -7,10 +7,27 @@
 #include <dm.h>
 #include <init.h>
 #include <dm/uclass-internal.h>
+#include <asm/io.h>
+
+#define TOP_SYSCLK_BASE				0x0013b000
+#define PLL_CPU_BUS_CFG0_REG	TOP_SYSCLK_BASE + 0x018
+#define PLL_CPU_BUS_CFG1_REG	TOP_SYSCLK_BASE + 0x01C
+
+#define PLL_CFG0_LOCK					BIT(30)
+
+static bool pll_unlocked(void) {
+	return (readl(PLL_CPU_BUS_CFG0_REG) & PLL_CFG0_LOCK) == 0;
+}
 
 int arch_cpu_init(void)
 {
 	icache_enable();
+
+	if (pll_unlocked()) {
+		writel(BIT(24), PLL_CPU_BUS_CFG1_REG);
+		writel(0x08348989, PLL_CPU_BUS_CFG0_REG);
+		while (pll_unlocked());
+	}
 
 	return 0;
 }
